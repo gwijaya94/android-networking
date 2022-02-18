30fe6575afc0fecf283761aefde8683498aca91f
Gilbert <gilbert.wijaya@ai.astra.co.id>
2022-02-19 00:55:30 +0700

    - ParseJsonArrayActivity layout and integrate with loopj for getting data - add QuoteAdapter

--- app/src/main/java/id/gwijaya94/networking/ParseJsonArrayActivity.kt
+++ app/src/main/java/id/gwijaya94/networking/ParseJsonArrayActivity.kt
@@ -1,11 +1,73 @@
 package id.gwijaya94.networking
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import id.gwijaya94.networking.databinding.ActivityParseJsonArrayBinding
+import org.json.JSONObject
 
 class ParseJsonArrayActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityParseJsonArrayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_parse_json_array)
+        binding = ActivityParseJsonArrayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getRandomQuote()
+    }
+
+    private fun getRandomQuote() {
+        binding.progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/random"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray
+            ) {
+                // Jika koneksi berhasil
+                binding.progressBar.visibility = View.INVISIBLE
+                val result = String(responseBody)
+                Log.d(TAG, result)
+                try {
+                    val responseObject = JSONObject(result)
+                    val quote = responseObject.getString("en")
+                    val author = responseObject.getString("author")
+                    binding.tvQuote.text = quote
+                    binding.tvAuthor.text = author
+                } catch (e: Exception) {
+                    Toast.makeText(this@ParseJsonArrayActivity, e.message, Toast.LENGTH_SHORT)
+                        .show()
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                // Jika koneksi gagal
+                binding.progressBar.visibility = View.INVISIBLE
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(this@ParseJsonArrayActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    companion object {
+        private val TAG = ParseJsonActivity::class.java.simpleName
     }
 }
